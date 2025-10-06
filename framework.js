200c8d2ce6072c01b730ab271a916042248c468f
Trofymenko Vladyslav <naughtysora@proton.me>
2025-10-06 21:16:33 +0300

    letters framework decomposition

--- framework.js
+++ framework.js
@@ -2,69 +2,27 @@
 
 const createDOMPurify = require('dompurify');
 const { JSDOM } = require('jsdom');
+const { resolve, sep } = require('node:path');
 const fs = require('node:fs');
-const { resolve } = require('node:path');
 const vm = require('node:vm');
 
-// idea is if input after sanitization has less length it means it was stripped so -> log it, security breach attempt
-
-class FrameWork {
-  #collection = new Map();
+class Sanitizer {
   #throwSanitize = false;
+  #purifier = createDOMPurify(new JSDOM('').window);
+  #options = null;
 
-  constructor({ throwSanitize = false } = {}) {
+  constructor({ throwSanitize = false, options } = {}) {
     this.#throwSanitize = throwSanitize;
+    this.#options = options ?? Sanitizer.#DEFAULT_OPTIONS;
   }
 
-  load(paths) {
-    for (const path of paths) {
-      const key = path.split("\\").at(-1);
-      const { script, styles } = this.#loadDir(path);
-      this.#collection.set(key, (props) => script({ ...props, styles, }));
-    }
-  }
-
-  letter(name, props) {
-    const letter = this.#collection.get(name);
-    if (letter === undefined) return;
-    if (typeof props !== "object" || props === null) {
-      throw new Error("Letter parameters have to an object");
-    }
-    return letter(this.#sanitize(props));
-  }
-
-  #loadDir(dir) {
-    const dataset = { 'index.html': null, 'styles.css': null };
-    for (const file of fs.readdirSync(dir)) {
-      if (!Object.hasOwn(dataset, file)) continue;
-      dataset[file] = fs.readFileSync(resolve(dir, file), "utf-8");
-    }
-    if (dataset["index.html"] === null || dataset["styles.css"] === null) {
-      throw new Error("Lack of info to create script for letters template");
-    }
-    return {
-      script: this.#createModule(dataset['index.html']),
-      styles: dataset["styles.css"],
-    }
-  }
-
-  #createModule(source) {
-    return new vm.Script("'use strict';\n(props) => (`" + source + "`);")
-      .runInNewContext(
-        FrameWork.#EMPTY_CONTEXT,
-        { timeout: FrameWork.#EXECUTION_TIMEOUT }
-      );
-  }
-
-  #sanitize(input) {
+  sanitize(input) {
     const output = {};
+    const options = this.#options;
     for (const name of Object.keys(input)) {
       const dirty = input[name];
       if (typeof dirty !== "string") continue;
-      const clean = FrameWork.#sanitizer.sanitize(
-        dirty,
-        { ALLOWED_TAGS: [], ALLOWED_ATTR: [], },
-      );
+      const clean = this.#purifier.sanitize(dirty, options);
       if (this.#throwSanitize && clean.length < dirty.length) {
         throw new Error(`HTML Injection, input: ${dirty}`);
       }
@@ -73,6 +31,10 @@ class FrameWork {
     return output;
   }
 
+  static #DEFAULT_OPTIONS = { ALLOWED_TAGS: [], ALLOWED_ATTR: [], };
+}
+
+class Realm {
   static #EXECUTION_TIMEOUT = 5000;
 
   static #EMPTY_CONTEXT = vm.createContext(Object.freeze({}), {
@@ -82,7 +44,59 @@ class FrameWork {
     },
   });
 
-  static #sanitizer = createDOMPurify(new JSDOM('').window);
+  create(source) {
+    return new vm.Script("'use strict';\n(props) => (`" + source + "`);")
+      .runInNewContext(
+        Realm.#EMPTY_CONTEXT,
+        { timeout: Realm.#EXECUTION_TIMEOUT }
+      );
+  }
+}
+
+class FrameWork {
+  #collection = new Map();
+  #sanitizer = null;
+  #realm = null;
+
+  constructor(options) {
+    this.#sanitizer = new Sanitizer(options);
+    this.#realm = new Realm();
+  }
+
+  load(paths) {
+    for (const path of paths) {
+      const key = path.split(sep).at(-1);
+      const { script, styles = "" } = this.#loadDir(path);
+      this.#collection.set(key, (props) => script({ ...props, styles, }));
+    }
+  }
+
+  letter(name, options) {
+    const letter = this.#collection.get(name);
+    if (letter === undefined) return;
+    if (typeof options !== "object" || options === null) {
+      throw new Error("Letter parameters have to an object");
+    }
+    return letter(this.#sanitizer.sanitize(options));
+  }
+
+  #loadDir(dir) {
+    const dataset = {
+      'index.html': undefined,
+      'styles.css': undefined
+    };
+    for (const file of fs.readdirSync(dir)) {
+      if (!Object.hasOwn(dataset, file)) continue;
+      dataset[file] = fs.readFileSync(resolve(dir, file), "utf-8");
+    }
+    if (!dataset["index.html"]) {
+      throw new Error("Can't find index.html file");
+    }
+    return {
+      script: this.#realm.create(dataset['index.html']),
+      styles: dataset["styles.css"],
+    }
+  }
 }
 
 const framework = new FrameWork({ throwSanitize: true });
