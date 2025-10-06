da01917d37fdc6ff8da5ef353f6f2c6aefa873a2
Trofymenko Vladyslav <naughtysora@proton.me>
2025-10-06 21:00:45 +0300

    testing framework sanitization

--- framework.js
+++ framework.js
@@ -1,13 +1,20 @@
 'use strict';
 
-// const createDOMPurify = require('dompurify');
-// const { JSDOM } = require('jsdom');
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
 const fs = require('node:fs');
 const { resolve } = require('node:path');
 const vm = require('node:vm');
 
+// idea is if input after sanitization has less length it means it was stripped so -> log it, security breach attempt
+
 class FrameWork {
   #collection = new Map();
+  #throwSanitize = false;
+
+  constructor({ throwSanitize = false } = {}) {
+    this.#throwSanitize = throwSanitize;
+  }
 
   load(paths) {
     for (const path of paths) {
@@ -15,7 +22,15 @@ class FrameWork {
       const { script, styles } = this.#loadDir(path);
       this.#collection.set(key, (props) => script({ ...props, styles, }));
     }
-    return this.#collection;
+  }
+
+  letter(name, props) {
+    const letter = this.#collection.get(name);
+    if (letter === undefined) return;
+    if (typeof props !== "object" || props === null) {
+      throw new Error("Letter parameters have to an object");
+    }
+    return letter(this.#sanitize(props));
   }
 
   #loadDir(dir) {
@@ -36,37 +51,41 @@ class FrameWork {
   #createModule(source) {
     return new vm.Script("'use strict';\n(props) => (`" + source + "`);")
       .runInNewContext(
-        FrameWork.EMPTY_CONTEXT,
-        { timeout: FrameWork.EXECUTION_TIMEOUT }
+        FrameWork.#EMPTY_CONTEXT,
+        { timeout: FrameWork.#EXECUTION_TIMEOUT }
       );
   }
 
-  static EXECUTION_TIMEOUT = 5000;
+  #sanitize(input) {
+    const output = {};
+    for (const name of Object.keys(input)) {
+      const dirty = input[name];
+      if (typeof dirty !== "string") continue;
+      const clean = FrameWork.#sanitizer.sanitize(
+        dirty,
+        { ALLOWED_TAGS: [], ALLOWED_ATTR: [], },
+      );
+      if (this.#throwSanitize && clean.length < dirty.length) {
+        throw new Error(`HTML Injection, input: ${dirty}`);
+      }
+      output[name] = clean;
+    }
+    return output;
+  }
 
-  static EMPTY_CONTEXT = vm.createContext(Object.freeze({}), {
+  static #EXECUTION_TIMEOUT = 5000;
+
+  static #EMPTY_CONTEXT = vm.createContext(Object.freeze({}), {
     codeGeneration: {
       strings: false,
       wasm: false,
     },
   });
+
+  static #sanitizer = createDOMPurify(new JSDOM('').window);
 }
 
-const framework = new FrameWork();
-
-const data = framework.load([
-  resolve(__dirname, "./reset-password"),
-]);
-
-console.log(data.get("reset-password")({ name: "Sora" }));
-
-// const window = new JSDOM('').window;
-// const DOMPurify = createDOMPurify(window);
-// const input = `<b>Hello</b> <script>alert('XSS')</script>`;
-// const input_good = "Hello <3";
-
-// idea is if input after sanitization has less length it means it was stripped so -> log it, security breach attempt
-// const clean = (input) =>
-//   DOMPurify.sanitize(input, { ALLOWED_TAGS: [], ALLOWED_ATTR: [], });
-
-// console.log({ input, clean: clean(input).trim() });
-// console.log({ input_good, clean: clean(input_good).trim(), });
+const framework = new FrameWork({ throwSanitize: true });
+framework.load([resolve(__dirname, "./reset-password"),]);
+const letter = framework.letter("reset-password", { name: "123456" });
+console.log(letter);
